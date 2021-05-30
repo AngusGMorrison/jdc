3e1b324202dfe44d03975d53bdc11af96660d0dd
Angus Morrison <angus.morrison.ext@qonto.com>
2021-05-30 11:21:01 +0100

    Correct hash syntax

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,13 +1,13 @@
 Sidekiq.configure_server do |config|
   config.redis = {
-    host: Rails.application.credentials.redis[:host]
+    host: Rails.application.credentials.redis[:host],
     port: Rails.application.credentials.redis[:port] || "6379"
   }
 end
 
 Sidekiq.configure_client do |config|
   config.redis = {
-    host: Rails.application.credentials.redis[:host]
+    host: Rails.application.credentials.redis[:host],
     port: Rails.application.credentials.redis[:port] || "6379"
   }
 end
