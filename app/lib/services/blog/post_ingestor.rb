66fd4ce5a697f79b7912c1249c4edb2de70fbc89
Angus Morrison <angus.morrison.ext@qonto.com>
2021-06-17 12:45:38 +0100

    Downgrade Ruby for upload to EB

--- app/lib/services/blog/post_ingestor.rb
+++ app/lib/services/blog/post_ingestor.rb
@@ -27,8 +27,6 @@ module Services
 
       BLOG_POST_FORMAT = /\A# (?<title>.+)\n\n!\[(?<alt_text>.*)\]\((?<image_path>.+)\)\n\n(?<body>.+)\z/m
 
-      # /\A# (?<title>.+)\n\n!\[(?<alt_text>.*)\]\((?<image_path>.+)\)\n\n(?<body>.+)\z/
-
       def self.ingest(path)
         new(path).ingest
       end
